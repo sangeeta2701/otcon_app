bf672f6a590f0034ed96b8cfc2ae4b3911812e27
DELL <anjugupta7060@gmail.com>
2022-11-02 09:57:20 +0530

    Otcon User App

--- lib/Convener/Home/status/presentation_status.dart
+++ lib/Convener/Home/status/presentation_status.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:otcon/utils/constants.dart';
 
 class PresentationStatusPage extends StatefulWidget {
   const PresentationStatusPage({Key? key}) : super(key: key);
@@ -10,9 +12,214 @@ class PresentationStatusPage extends StatefulWidget {
 class _PresentationStatusPageState extends State<PresentationStatusPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text("Status"),
+    return Scaffold(
+      backgroundColor: appUiLightColor,
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(20.0, 60.0, 20.0, 20.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                ""
+                "Presentation Status",
+                style: GoogleFonts.poppins(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                  color: appUiDarkColor,
+                  decoration: TextDecoration.none,
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              // Row(
+              //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              //   children: [
+              //     Text(
+              //       "Track",
+              //       style: GoogleFonts.poppins(
+              //         fontSize: 18,
+              //         fontWeight: FontWeight.w600,
+              //         color: appUiDarkColor,
+              //         decoration: TextDecoration.none,
+              //       ),
+              //     ),
+              //     Text(
+              //       "Completed",
+              //       style: GoogleFonts.poppins(
+              //         fontSize: 18,
+              //         fontWeight: FontWeight.w600,
+              //         color: appUiDarkColor,
+              //         decoration: TextDecoration.none,
+              //       ),
+              //     ),
+              //     Text(
+              //       "Pending",
+              //       style: GoogleFonts.poppins(
+              //         fontSize: 18,
+              //         fontWeight: FontWeight.w600,
+              //         color: appUiDarkColor,
+              //         decoration: TextDecoration.none,
+              //       ),
+              //     )
+              //   ],
+              // ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Track",
+                        style: GoogleFonts.poppins(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w600,
+                          color: appUiDarkColor,
+                          decoration: TextDecoration.none,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Text(
+                        "Emerging And\nIntelligent\nComputing",
+                        style: GoogleFonts.poppins(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400,
+                          color: appUiDarkColor,
+                          decoration: TextDecoration.none,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Text(
+                        "Communication\nControl And\nNetworking",
+                        style: GoogleFonts.poppins(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400,
+                          color: appUiDarkColor,
+                          decoration: TextDecoration.none,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Text(
+                        "Renewable\nEnergy And\nPower System",
+                        style: GoogleFonts.poppins(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400,
+                          color: appUiDarkColor,
+                          decoration: TextDecoration.none,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Text(
+                        "Bio Informatics\nAnd Health Care",
+                        style: GoogleFonts.poppins(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400,
+                          color: appUiDarkColor,
+                          decoration: TextDecoration.none,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Text(
+                        "IOT Automation\nAnd Manufacturing",
+                        style: GoogleFonts.poppins(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400,
+                          color: appUiDarkColor,
+                          decoration: TextDecoration.none,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Text(
+                        "Signal And\nImage Processing",
+                        style: GoogleFonts.poppins(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400,
+                          color: appUiDarkColor,
+                          decoration: TextDecoration.none,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Text(
+                        "Cyber Physical\nSystem And\nMetaverse",
+                        style: GoogleFonts.poppins(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400,
+                          color: appUiDarkColor,
+                          decoration: TextDecoration.none,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Text(
+                        "Education\nEnviornment And\nEconomics",
+                        style: GoogleFonts.poppins(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400,
+                          color: appUiDarkColor,
+                          decoration: TextDecoration.none,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Completed",
+                        style: GoogleFonts.poppins(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w600,
+                          color: appUiDarkColor,
+                          decoration: TextDecoration.none,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Container(
+                        height: 50,
+                        width: 60,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: appUigreyColor,
+                        ),
+                        child: Center(
+                          child: Text(
+                            "3",
+                            style: GoogleFonts.poppins(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600,
+                              color: appUiOrangekColor,
+                              decoration: TextDecoration.none,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
